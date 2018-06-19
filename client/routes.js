f29de9cac1a942563a7e40471d09621b6e2bb695
Sythrill <ksciwiarska@gmail.com>
2018-06-19 16:25:20 +0200

    create navigation and add home and about pages

--- client/routes.js
+++ client/routes.js
@@ -1,6 +1,6 @@
 /* eslint-disable global-require */
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import {Route, IndexRoute} from 'react-router';
 import App from './modules/App/App';
 
 // require.ensure polyfill for node
@@ -18,6 +18,8 @@ if (process.env.NODE_ENV !== 'production') {
   // Require async routes only in development for react-hot-reloader to work.
   require('./modules/Post/pages/PostListPage/PostListPage');
   require('./modules/Post/pages/PostDetailPage/PostDetailPage');
+  require('./modules/Home/Home');
+  require('./modules/About/About');
 }
 
 // react-router setup with code-splitting
@@ -39,5 +41,22 @@ export default (
         });
       }}
     />
+    <Route
+      path="/home"
+      getComponent={(nextState, cb) => {
+        require.ensure([], require => {
+          cb(null, require('./modules/Home/Home').default);
+        });
+      }}
+    />
+    <Route
+      path="/about"
+      getComponent={(nextState, cb) => {
+        require.ensure([], require => {
+          cb(null, require('./modules/About/About').default);
+        });
+      }}
+    />
   </Route>
+
 );
